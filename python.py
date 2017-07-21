8ec11c2cb6dc84f1eed2a17ad534dcd0368060c2
Bdog27 <cowardbri@gmail.com>
2017-07-21 15:42:30 -0400

    Add files via upload

--- python.py
+++ python.py
@@ -21,8 +21,8 @@ absolute = abs(-554)
 print (absolute)
 print (type("alex"))"""
 
-
-def hotel_cost(days):
+#hotel ,plane and car costs
+"""def hotel_cost(days):
     return days*150
 
 def plane_cost(city):
@@ -34,7 +34,6 @@ def plane_cost(city):
         return 220
     elif city == "Tokyo":
         return 475
-
 def car_cost(days):
     price=150*days
     if days >= 7:
@@ -46,9 +45,57 @@ def car_cost(days):
 
 def total_cost():
     askcity = input("what city would you like to stay in")
-    askdays = int(input("How many days would you like to stay?"))
-    hotelcost = hotel_cost(askdays)
-    planecost = plane_cost(askcity)
-    carcost = car_cost(askdays)
-    total = hotelcost + planecost + carcost
-    return(total)
+    if askcity == "Chicago" or askcity == "London" or askcity == "Tokyo" or askcity == "Toronto":
+        askdays = int(input("How many days would you like to stay?"))
+        hotelcost = hotel_cost(askdays)
+        planecost = plane_cost(askcity)
+        carcost = car_cost(askdays)
+        total = hotelcost + planecost + carcost
+        return(total)
+    else:
+        print ("please enter a valid city")
+        total_cost()
+total_cost()"""
+
+
+"""numbers = [1,2,3,4,5,6,7,8,9,10]
+
+numbers[8] = 21
+
+numbers.append(123456789)"""
+
+"""letters = [ "a","b","c","d","e","f"]
+
+sliced_list = letters[2:5]
+print (sliced_list)
+print (letters)"""
+
+"""aminals = ["ant","bat","cat","dog"]
+print (aminals.index("cat"))
+
+
+aminals.insert(1,"snake")
+print (aminals)
+
+aminals.pop(2)# gets rid of item using inedx number
+print (aminals)
+
+aminals.remove("dog")
+print (aminals)
+
+aminals.sort()
+print (aminals)"""
+
+
+numbers = [1,2,3,4,5,6,7,8,9]
+def print_first(lists):
+    return(lists[0])
+
+
+
+
+
+
+
+
+
